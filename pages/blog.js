4d50214040c1f97fe80ac6cec7a64c2fad816921
Fr3shBrain <aylanc.onur@gmail.com>
2021-10-07 22:20:29 +0300

    globals.css eklendi.

---
+++ pages/blog.js
@@ -0,0 +1,5 @@
+const BlogPage = () => {
+  return <div className="site-container"><h1 className="text-2xl font-bold">Yazılar</h1></div>
+}
+
+export default BlogPage
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 function HomePage() {
-  return <div className="text-purple-900 bg-blue-200">Welcome to Next.js!</div>;
+  return (
+    <>
+      <div className="site-container">
+        <div className="space-y-4">
+          <h1 className="text-2xl font-bold">
+            Ben Onur Aylanç. İstanbul'da yaşayan Frontend Developer'ım.
+          </h1>
+          <p className="">
+            Tasarım araçları, front-end teknolojileri, sokak fotoğrafçılığı ve
+            tipografi gibi konularla yakından ilgileniyorum.
+          </p>
+          <p className="">
+            Sektördeki eski teknoloji ve alışkanlıkları yenilerle değiştirmek
+            için youtube kanalımda modern türkçe içerikler üretiyorum.
+          </p>
+        </div>
+      </div>
+      <div className="max-w-4xl mx-auto mt-10">
+        <img src="/js-return.jpg" />
+      </div>
+    </>
+  )
 }
 
-export default HomePage;
+export default HomePage
